1ba1dc7156fe3c92175561889930cf27d5478237
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-27 15:31:32 +0700

    create a data source where data is collected from remote

---
+++ core/src/main/java/id/rsdiz/core/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,57 @@
+package id.rsdiz.core.data.source.remote
+
+import id.rsdiz.core.data.source.remote.network.ApiResponse
+import id.rsdiz.core.data.source.remote.network.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
+
+    suspend fun getListGames() =
+        flow {
+            try {
+                val response = apiService.getListGames()
+                if (response.results.isNotEmpty()) emit(ApiResponse.Success(response.results))
+                else emit(ApiResponse.Empty)
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.localizedMessage ?: e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+
+    fun searchGame(query: String) =
+        flow {
+            try {
+                val response = apiService.searchGames(query)
+                if (response.results.isNotEmpty()) emit(ApiResponse.Success(response.results))
+                else emit(ApiResponse.Empty)
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+
+    suspend fun getDetailGame(id: Int) =
+        flow {
+            try {
+                val response = apiService.getGames(id)
+                if (response.description?.isNotEmpty() == true) emit(ApiResponse.Success(response))
+                else emit(ApiResponse.Empty)
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+
+    suspend fun getGamesByDeveloper(developer: String) =
+        flow {
+            try {
+                val response = apiService.getGamesByDeveloper(developer)
+                if (response.results.isNotEmpty()) emit(ApiResponse.Success(response.results))
+                else emit(ApiResponse.Empty)
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+}
