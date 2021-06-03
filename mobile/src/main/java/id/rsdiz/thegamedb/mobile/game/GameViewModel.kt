ff5622aedcda121d63485ffeae2ca8f417dff971
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-03 18:36:58 +0700

    implement view model to get all game data

--- mobile/src/main/java/id/rsdiz/thegamedb/mobile/game/GameViewModel.kt
+++ mobile/src/main/java/id/rsdiz/thegamedb/mobile/game/GameViewModel.kt
@@ -1,7 +1,12 @@
 package id.rsdiz.thegamedb.mobile.game
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.rsdiz.thegamedb.core.domain.usecase.GameUseCase
+import javax.inject.Inject
 
-class GameViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class GameViewModel @Inject constructor(gameUseCase: GameUseCase) : ViewModel() {
+    val games = gameUseCase.getGames().asLiveData()
 }
