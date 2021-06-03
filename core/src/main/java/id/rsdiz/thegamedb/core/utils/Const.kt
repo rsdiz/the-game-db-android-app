06469d350d90d610f62b681963b73a9cdc44f063
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-03 18:21:51 +0700

    add constanta

--- core/src/main/java/id/rsdiz/thegamedb/core/utils/Const.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/utils/Const.kt
@@ -1,7 +1,19 @@
 package id.rsdiz.thegamedb.core.utils
 
+import id.rsdiz.thegamedb.core.BuildConfig
+
 object Const {
     const val DB_NAME = "Games"
     const val TIMEOUT = 120L
     const val RAWG_API_BASE_URL = "https://api.rawg.io/api/"
+    const val RAWG_API_KEY = BuildConfig.API_KEY
+    const val PARAM_KEY = "key"
+    const val PC = "pc"
+    const val XBOX = "xbox"
+    const val PLAYSTATION = "playstation"
+    const val ANDROID = "android"
+    const val LINUX = "linux"
+    const val IOS = "ios"
+    const val MAC = "apple macintosh"
+    const val NINTENDO = "nintendo"
 }
