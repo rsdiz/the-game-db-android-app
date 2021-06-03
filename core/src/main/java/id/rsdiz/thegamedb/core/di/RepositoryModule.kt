e8c95d48aaa510f910cf88e54378e8b4f01e7803
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-03 18:00:18 +0700

    change wrong interface

--- core/src/main/java/id/rsdiz/thegamedb/core/di/RepositoryModule.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/di/RepositoryModule.kt
@@ -3,12 +3,13 @@ package id.rsdiz.thegamedb.core.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import id.rsdiz.thegamedb.core.data.GameRepository
-import javax.inject.Singleton
+import id.rsdiz.thegamedb.core.domain.repository.IGameRepository
 
 @Module(includes = [NetworkModule::class])
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     @Binds
-    abstract fun provideGameRepository(repository: GameRepository): GameRepository
+    abstract fun provideGameRepository(repository: GameRepository): IGameRepository
 }
