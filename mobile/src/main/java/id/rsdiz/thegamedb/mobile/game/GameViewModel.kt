8132d676005f5822de549d0d3a600968228641ee
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-04 13:48:28 +0700

    add function to search game

--- mobile/src/main/java/id/rsdiz/thegamedb/mobile/game/GameViewModel.kt
+++ mobile/src/main/java/id/rsdiz/thegamedb/mobile/game/GameViewModel.kt
@@ -1,12 +1,23 @@
 package id.rsdiz.thegamedb.mobile.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import id.rsdiz.thegamedb.core.data.Resource
+import id.rsdiz.thegamedb.core.domain.model.Game
 import id.rsdiz.thegamedb.core.domain.usecase.GameUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class GameViewModel @Inject constructor(gameUseCase: GameUseCase) : ViewModel() {
+class GameViewModel @Inject constructor(private val gameUseCase: GameUseCase) : ViewModel() {
     val games = gameUseCase.getGames().asLiveData()
+
+    private var _searchResult = MutableLiveData<Resource<List<Game>>>()
+    val searchResult: LiveData<Resource<List<Game>>> get() = _searchResult
+
+    suspend fun searchGames(query: String) {
+        _searchResult.value = gameUseCase.searchGame(query)
+    }
 }
