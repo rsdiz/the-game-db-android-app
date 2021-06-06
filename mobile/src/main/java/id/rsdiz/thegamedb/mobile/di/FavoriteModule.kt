a07211c8d74983c5718cc27d79dd377cef60a4a4
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-06 10:14:17 +0700

    add dependency injection for favorite module

---
+++ mobile/src/main/java/id/rsdiz/thegamedb/mobile/di/FavoriteModule.kt
@@ -0,0 +1,12 @@
+package id.rsdiz.thegamedb.mobile.di
+
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import id.rsdiz.thegamedb.core.domain.usecase.GameUseCase
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface FavoriteModule {
+    fun gameUseCase(): GameUseCase
+}
