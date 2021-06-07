2838dde7d20fdfb6c8091af078097390b3b3b7ca
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-08 02:45:10 +0700

    implement the certificate pinning technique

--- core/src/main/java/id/rsdiz/thegamedb/core/di/NetworkModule.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.components.SingletonComponent
 import id.rsdiz.thegamedb.core.data.source.remote.network.ApiService
 import id.rsdiz.thegamedb.core.utils.Const
 import id.rsdiz.thegamedb.core.utils.ParameterInterceptor
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,13 +18,25 @@ import java.util.concurrent.TimeUnit
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
     @Provides
-    fun provideOkHttpClient() =
-        OkHttpClient.Builder()
+    fun provideOkHttpClient(): OkHttpClient {
+        val rawgCertPinner = CertificatePinner.Builder()
+            .add(
+                Const.RAWG_HOSTNAME,
+                Const.RAWG_SSL_PIN[0],
+                Const.RAWG_SSL_PIN[1],
+                Const.RAWG_SSL_PIN[2],
+                Const.RAWG_SSL_PIN[3]
+            )
+            .build()
+
+        return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor(ParameterInterceptor(Const.RAWG_API_KEY))
             .connectTimeout(Const.TIMEOUT, TimeUnit.SECONDS)
             .readTimeout(Const.TIMEOUT, TimeUnit.SECONDS)
+            .certificatePinner(rawgCertPinner)
             .build()
+    }
 
     @Provides
     fun provideApiService(client: OkHttpClient): ApiService =
--- core/src/main/java/id/rsdiz/thegamedb/core/utils/Const.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/utils/Const.kt
@@ -7,7 +7,14 @@ object Const {
     const val PASSPHRASE = "TheGameDB"
     const val TIMEOUT = 120L
     const val RAWG_API_BASE_URL = "https://api.rawg.io/api/"
+    const val RAWG_HOSTNAME = "api.rawg.io"
     const val RAWG_API_KEY = BuildConfig.API_KEY
+    val RAWG_SSL_PIN = arrayOf(
+        BuildConfig.PIN_SSL_1,
+        BuildConfig.PIN_SSL_2,
+        BuildConfig.PIN_SSL_3,
+        BuildConfig.PIN_SSL_4
+    )
     const val PARAM_KEY = "key"
     const val PC = "pc"
     const val XBOX = "xbox"
