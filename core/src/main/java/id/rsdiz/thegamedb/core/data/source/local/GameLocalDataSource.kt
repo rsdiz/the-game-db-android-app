b34644fa6234d17e39fae9e3857e0d532667cf6f
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-07 10:58:06 +0700

    rename file and add mapper

--- core/src/main/java/id/rsdiz/thegamedb/core/data/source/local/GameLocalDataSource.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/data/source/local/GameLocalDataSource.kt
@@ -1,25 +1,27 @@
 package id.rsdiz.thegamedb.core.data.source.local
 
 import id.rsdiz.thegamedb.core.data.source.local.entity.GameEntity
+import id.rsdiz.thegamedb.core.data.source.local.mapper.GameMapper
 import id.rsdiz.thegamedb.core.data.source.local.room.IGameDao
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class LocalDataSource @Inject constructor(private val gameDao: IGameDao) {
+class GameLocalDataSource @Inject constructor(
+    private val gameDao: IGameDao,
+    val mapper: GameMapper
+) {
     fun getAllGames() = gameDao.getAllGames()
 
     fun getFavoriteGames() = gameDao.getFavoriteGames()
 
     fun getGameById(id: Int) = gameDao.getGameById(id)
 
-    fun getDevelopersList() = gameDao.getAllDevelopers()
-
     fun getListGameByDeveloper(developerName: String) = gameDao.getGamesByDeveloper(developerName)
 
-    fun updateGame(data: GameEntity) = gameDao.update(data)
+    fun update(data: GameEntity) = gameDao.update(data)
 
-    suspend fun insertGame(data: GameEntity) = gameDao.insert(data)
+    suspend fun insert(data: GameEntity) = gameDao.insert(data)
 
-    suspend fun insertAllGame(list: List<GameEntity>) = gameDao.insertAll(list)
+    suspend fun insertAll(list: List<GameEntity>) = gameDao.insertAll(list)
 }
