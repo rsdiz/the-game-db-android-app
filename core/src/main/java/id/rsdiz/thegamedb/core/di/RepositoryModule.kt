fbde3ef89706ebbed39b02b514075f2b6c3cbe04
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-07 11:12:56 +0700

    provide developers repository in RepositoryModule.kt

--- core/src/main/java/id/rsdiz/thegamedb/core/di/RepositoryModule.kt
+++ core/src/main/java/id/rsdiz/thegamedb/core/di/RepositoryModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import id.rsdiz.thegamedb.core.data.GameRepository
+import id.rsdiz.thegamedb.core.data.repository.DevelopersRepository
+import id.rsdiz.thegamedb.core.data.repository.GameRepository
+import id.rsdiz.thegamedb.core.domain.repository.IDevelopersRepository
 import id.rsdiz.thegamedb.core.domain.repository.IGameRepository
 
 @Module(includes = [NetworkModule::class])
@@ -12,4 +14,7 @@ import id.rsdiz.thegamedb.core.domain.repository.IGameRepository
 abstract class RepositoryModule {
     @Binds
     abstract fun provideGameRepository(repository: GameRepository): IGameRepository
+
+    @Binds
+    abstract fun provideDevelopersRepository(repository: DevelopersRepository): IDevelopersRepository
 }
