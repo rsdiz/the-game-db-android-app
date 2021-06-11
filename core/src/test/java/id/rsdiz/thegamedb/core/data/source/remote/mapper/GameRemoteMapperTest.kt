28f6cbfcbc78e5a38ca76ffe8c1e720357a70857
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-11 17:45:40 +0700

    create unit test for GameRemoteMapper

---
+++ core/src/test/java/id/rsdiz/thegamedb/core/data/source/remote/mapper/GameRemoteMapperTest.kt
@@ -0,0 +1,48 @@
+package id.rsdiz.thegamedb.core.data.source.remote.mapper
+
+import id.rsdiz.thegamedb.core.data.source.local.entity.GameEntity
+import id.rsdiz.thegamedb.core.data.source.remote.response.games.GameResponse
+import id.rsdiz.thegamedb.core.factory.GameFactory
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class GameRemoteMapperTest {
+    private lateinit var gameRemoteMapper: GameRemoteMapper
+
+    @Before
+    fun setUp() {
+        gameRemoteMapper = GameRemoteMapper()
+    }
+
+    @Test
+    fun mapRemoteToEntity() {
+        val gameRemote = GameFactory.makeGameResponse()
+        val gameEntity = gameRemoteMapper.mapRemoteToEntity(gameRemote)
+
+        assertGameDataEquality(gameRemote, gameEntity)
+    }
+
+    @Test
+    fun mapRemoteToEntities() {
+        val gameRemoteList = GameFactory.makeGameResponseList(2)
+        val gameEntities = gameRemoteMapper.mapRemoteToEntities(gameRemoteList)
+
+        repeat(gameRemoteList.size) {
+            assertGameDataEquality(gameRemoteList[it], gameEntities[it])
+        }
+    }
+
+    private fun assertGameDataEquality(entity: GameResponse, data: GameEntity) {
+        assertEquals(entity.id, data.id)
+        assertEquals(entity.name, data.name)
+        assertEquals(entity.backgroundImage, data.backgroundImage)
+        assertEquals(entity.rating, data.rating)
+        assertEquals(entity.released, data.released)
+        assertEquals(entity.websiteUrl, data.websiteUrl)
+        assertEquals(entity.playtime, data.playtime)
+    }
+}
