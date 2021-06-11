5c8a28532640b153cbb2615d8f97bafcc555da25
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-11 17:44:20 +0700

    create unit test for GameEntity

---
+++ core/src/test/java/id/rsdiz/thegamedb/core/data/source/local/entity/GameEntityTest.kt
@@ -0,0 +1,46 @@
+package id.rsdiz.thegamedb.core.data.source.local.entity
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class GameEntityTest {
+    @Test
+    fun testEqualsSymetric() {
+        val game1 = GameEntity(
+            1,
+            "Name",
+            "Background",
+            5.0f,
+            "ParentPlatform",
+            "Genres",
+            "2021-11-06",
+            "github.com",
+            10,
+            "Platforms",
+            "Developers",
+            "Description",
+            true
+        )
+        val game2 = GameEntity(
+            1,
+            "Name",
+            "Background",
+            5.0f,
+            "ParentPlatform",
+            "Genres",
+            "2021-11-06",
+            "github.com",
+            10,
+            "Platforms",
+            "Developers",
+            "Description",
+            true
+        )
+
+        assertTrue(game1 == game2 && game2 == game1)
+        assertTrue(game1.hashCode() == game2.hashCode())
+    }
+}
