b10b0da7531aa8a30961d33e775c8486091e225f
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-12 13:42:16 +0700

    add unit test utility for favorite module

---
+++ favorite/src/test/java/id/rsdiz/thegamedb/mobile/favorite/factory/GameFactory.kt
@@ -0,0 +1,32 @@
+package id.rsdiz.thegamedb.mobile.favorite.factory
+
+import id.rsdiz.thegamedb.core.domain.model.Game
+
+class GameFactory {
+    companion object Factory {
+        fun makeGame(): Game =
+            Game(
+                DataFactory.randomInt(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomDouble().toFloat(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomInt(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomUuid(),
+                DataFactory.randomBoolean()
+            )
+
+        fun makeGameList(count: Int): List<Game> {
+            val gameList = mutableListOf<Game>()
+            repeat(count) {
+                gameList.add(makeGame())
+            }
+            return gameList
+        }
+    }
+}
