5ad22b022b1a52d4fda0f618f5a96d9000ba39c2
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-12 13:43:05 +0700

    add unit test for FavoriteViewModel

---
+++ favorite/src/test/java/id/rsdiz/thegamedb/mobile/favorite/vm/FavoriteViewModelTest.kt
@@ -0,0 +1,63 @@
+package id.rsdiz.thegamedb.mobile.favorite.vm
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import id.rsdiz.thegamedb.core.domain.model.Game
+import id.rsdiz.thegamedb.core.domain.usecase.game.GameUseCase
+import id.rsdiz.thegamedb.mobile.favorite.MainCoroutineScopeRule
+import id.rsdiz.thegamedb.mobile.favorite.factory.GameFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class FavoriteViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
+
+    private val fakeGameList = GameFactory.makeGameList(10)
+    private val favoritedGames = fakeGameList.filter { game -> game.isFavorite }
+    private lateinit var favoriteViewModel: FavoriteViewModel
+
+    @Mock
+    private lateinit var gameUseCase: GameUseCase
+
+    @Mock
+    private lateinit var gamesObserver: Observer<List<Game>>
+
+    @Captor
+    private lateinit var captorGames: ArgumentCaptor<List<Game>>
+
+    @Before
+    fun setUp() {
+        val flow: Flow<List<Game>> = flow { emit(favoritedGames) }
+
+        Mockito.`when`(gameUseCase.getFavoriteGames()).thenReturn(flow)
+        favoriteViewModel = FavoriteViewModel(gameUseCase)
+        Mockito.verify(gameUseCase).getFavoriteGames()
+    }
+
+    @Test
+    fun getFavoriteGames() {
+        val liveData = favoriteViewModel.favoriteGames
+        liveData.observeForever(gamesObserver)
+
+        Mockito.verify(gamesObserver).onChanged(captorGames.capture())
+        assertNotNull(captorGames.value)
+        assertEquals(favoritedGames, captorGames.value)
+    }
+}
